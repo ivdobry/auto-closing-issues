08f577d8ac879b7d0afe9c9441eef01972f9dc58
ivdobry <ivan@rzilient.club>
2022-04-08 17:01:27 +0200

    first comit loool

---
+++ index.js
@@ -0,0 +1,22 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const autoCLosingIssues = async () => {
+  try {
+    const repo = core.getInput('repo', { required: true });
+    const prName = core.getInput('prName', { required: true });
+    const token = core.getInput('token', { required: true });
+
+    const octokit = new github.getOctokit(token);
+
+    const prNameSplited = prName.split('/');
+
+    if (!prNameSplited[0] === 'issue') return;
+
+    octokit.rest.issues.close_issue(repo, prNameSplited[1].split('-')[0]);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+autoCLosingIssues();
